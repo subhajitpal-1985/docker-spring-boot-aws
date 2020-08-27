c51ad1584a9c8d3d807e8e5c26d0054f0f9dcd2c
Subhajit Pal <subhajit.pal-cic.germany@ibm.com>
2020-08-27 12:50:12 +0200

    first commit

---
+++ src/main/java/com/example/docker/aws/dockerspringbootaws/DockerSpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker.aws.dockerspringbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker/aws/dockerspringbootaws/DockerSpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.aws.dockerspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
